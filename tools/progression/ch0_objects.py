4fbab979bc9e97987637a57eca966fa218fb6af4
Icebound777 <frozenblue@gmx.de>
2021-05-11 00:10:48 +0200

    corrected ch0, ch1

--- tools/progression/ch0_objects.py
+++ tools/progression/ch0_objects.py
@@ -1,36 +1,45 @@
-locations = [
-#map          check               index item            removes     requires     
-{"kmr_00":  [{"Panel":           (255,  "StarPiece",    None,       [("SuperHammer", "SuperBoots"), ("UltraHammer",)])}]},
+locations = {
+#map          check             item            requires  
+"kmr_00":  [("Panel",           "StarPiece",    [("SuperHammer", "SuperBoots"), ("UltraHammer",)]),
+            ],
 
-{"kmr_04":  [{"Tree":            (?,    "Dolly",        None,       [("SuperHammer",)])}]},
-{"kmr_04":  [{"Bush":            (?,    "Hammer",       None,       [("SuperHammer",)])}]}, # awesome...
+"kmr_04":  [("Tree",            "Dolly",        [("SuperHammer",)]),
+            ("Bush",            "Hammer",       [("SuperHammer",)]),  # awesome...
+            ],
 
-{"kmr_02":  [{"Goompa":          (?,    "PowerJump",    None,       [("SuperHammer",)])}]},
-{"kmr_02":  [{"GoompaKKF":       (?,    "Tape",         None,       [("SuperHammer", "KKF03")])}]},
-{"kmr_02":  [{"Dolly":           (?,    "StarPiece",    "Dolly",    [("SuperHammer", "Dolly")])}]},
-{"kmr_02":  [{"Veranda":         (?,    "ShootingStar", None,       [("SuperHammer", "GF_GoombaKing")])}]},
-{"kmr_02":  [{"Tree":            (?,    "Goomnut",      None,       [("SuperHammer")])}]},
-{"kmr_02":  [{"GoompaLetter":    (?,    "StarPiece",    "Letter02", [("SuperHammer", "Parakarry", "Letter02")])}]},
-{"kmr_02":  [{"GoompapaLetter1": (?,    "Letter13",     "Letter10", [("SuperHammer", "Parakarry", "Letter10")])}]},
-{"kmr_02":  [{"GoompapaLetter2": (?,    "LuckyDay",     "Letter24", [("SuperHammer", "Parakarry", "Letter24")])}]},
+"kmr_02":  [("Goompa",          "PowerJump",    [("SuperHammer",)]),
+            ("GoompaKKF",       "Tape",         [("SuperHammer", "KKF03")]),
+            ("Dolly",           "StarPiece",    [("SuperHammer", "Dolly")]),
+            ("Veranda",         "ShootingStar", [("SuperHammer", "GF_GoombaKing")]),
+            ("Tree",            "Goomnut",      [("SuperHammer")]),
+            ("GoompaLetter",    "StarPiece",    [("SuperHammer", "Parakarry", "Letter02")]),
+            ("GoompapaLetter1", "Letter13",     [("SuperHammer", "Parakarry", "Letter10")]),
+            ("GoompapaLetter2", "LuckyDay",     [("SuperHammer", "Parakarry", "Letter24")]),
+            ],
 
-{"kmr_03":  [{"Ledge":           (?,    "FireFlower",   None,       [("SuperHammer",)])}]},
-{"kmr_03":  [{"Tree":            (?,    "Mushroom",     None,       [("SuperHammer",)])}]},
-{"kmr_03":  [{"Hidden?Block":    (?,    "RepelGel",     None,       [("SuperHammer",)])}]},
-{"kmr_03":  [{"Panel":           (?,    "StarPiece",    None,       [("SuperHammer", "SuperBoots"), ("UltraHammer",)])}]},
+"kmr_03":  [("Ledge",           "FireFlower",   [("SuperHammer",)]),
+            ("Tree",            "Mushroom",     [("SuperHammer",)]),
+            ("Hidden?Block",    "RepelGel",     [("SuperHammer",)]),
+            ("Panel",           "StarPiece",    [("SuperHammer", "SuperBoots"), ("UltraHammer",)]),
+            ],
 
-{"kmr_05":  [{"Ledge":           (?,    "StarPiece",    None,       [("SuperHammer",)])}]},
+"kmr_05":  [("Ledge",           "StarPiece",    [("SuperHammer",)]),
+            ],
 
-{"kmr_06":  [{"Sign":            (?,    "Mushroom",     None,       [("SuperHammer",)])}]},
-{"kmr_06":  [{"Red?Block":       (?,    "CloseCall",    None,       [("SuperHammer",)])}]},
+"kmr_06":  [("Sign",            "Mushroom",     [("SuperHammer",)]),
+            ("Red?Block",       "CloseCall",    [("SuperHammer",)]),
+            ],
 
-{"kmr_11":  [{"Tree":            (?,    "StarPiece",    None,       [("SuperHammer",)])}]},
-{"kmr_11":  [{"Hammerblock":     (?,    "SuperShroom",  None,       [("SuperHammer",)])}]},
-{"kmr_11":  [{"Panel":           (?,    "StarPiece",    None,       [("SuperHammer", "SuperBoots"), ("UltraHammer",)])}]},
+"kmr_11":  [("Tree",            "StarPiece",    [("SuperHammer",)]),
+            ("Hammerblock",     "SuperShroom",  [("SuperHammer",)]),
+            ("Panel",           "StarPiece",    [("SuperHammer", "SuperBoots"), ("UltraHammer",)]),
+            ],
 
-{"kmr_10":  [{"Yellow?Block":    (?,    "SleepySheep",  None,       [])}]},
-{"kmr_10":  [{"Chest":           (?,    "HammerThrow",  None,       [("SuperHammer",)])}]},
+"kmr_10":  [("Yellow?Block",    "SleepySheep",  None),
+            ("Chest",           "HammerThrow",  [("SuperHammer",)]),
+            ],
 
-#{"kmr_20":  [{"Luigi":           (?,    "Autohraph1?",  None,       [("KKF_05",)])}]},
+#"kmr_20":  [("Luigi",           "Autohraph1?",  [("KKF_05",)]),
+#            ],
 
-]
+}
