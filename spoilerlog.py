868c519ad5628a1e09ccf671009ce080cd944d8d
Pronyo <franktb93@gmail.com>
2022-02-01 07:45:40 -0500

    Spoiler Log: Write to a IO memory file during web execution

--- spoilerlog.py
+++ spoilerlog.py
@@ -1,3 +1,4 @@
+import io
 import os
 from metadata.verbose_area_names import verbose_area_names
 from metadata.verbose_item_names import verbose_item_names
@@ -9,53 +10,63 @@ def write_spoiler_log(
     placed_items:list,
     random_chapter_difficulty:dict=None,
     settings:OptionSet=False,
-    spoilerlog_file:str=None
+    spoilerlog_file:str=None,
+    is_web_spoiler_log = False
 ):
     """
     Outputs a log file listing the final locations of all items
     after randomization and randomized chapter difficulty
     """
-    if not spoilerlog_file:
+    if not spoilerlog_file and not is_web_spoiler_log:
         spoilerlog_file = os.path.abspath(__file__ + "/../debug/item_placement.txt")
+        
 
     sorted_by_key =  sorted(placed_items,  key=lambda node: node.key_name_item)
     sorted_by_map =  sorted(sorted_by_key, key=lambda node: node.map_area.map_id)
     sorted_by_area = sorted(sorted_by_map, key=lambda node: node.map_area.area_id)
-    with open(spoilerlog_file, "w", encoding="utf-8") as file:
-        # Print chapter difficulties
-        if random_chapter_difficulty and not settings.progressive_scaling:
-            file.write("Modified Chapter Difficulty:")
-            for old_chapter, new_chapter in random_chapter_difficulty.items():
-                file.write(f"\nChapter {old_chapter} -> Chapter {new_chapter}")
-            file.write("\n\n")
 
-        # Print item locations
-        if settings.pretty_spoilerlog:
-            current_area_name = None
-            for node in sorted_by_area:
-                new_area_name = verbose_area_names.get(node.map_area.name[:3])
-                if (current_area_name is None):
-                    file.write(f"{new_area_name}:\n")
-                elif (new_area_name != current_area_name):
-                    file.write(f"\n{new_area_name}:\n")
+    if is_web_spoiler_log:
+        file = io.StringIO()
+    else:
+        file = open(spoilerlog_file, "w", encoding="utf-8")
+    # Print chapter difficulties
+    if random_chapter_difficulty and not settings.progressive_scaling:
+        file.write("Modified Chapter Difficulty:")
+        for old_chapter, new_chapter in random_chapter_difficulty.items():
+            file.write(f"\nChapter {old_chapter} -> Chapter {new_chapter}")
+        file.write("\n\n")
 
-                current_item_name = node.current_item.item_name
-                map_verbose_name = node.map_area.verbose_name
+    # Print item locations
+    if settings.pretty_spoilerlog:
+        current_area_name = None
+        for node in sorted_by_area:
+            new_area_name = verbose_area_names.get(node.map_area.name[:3])
+            if (current_area_name is None):
+                file.write(f"{new_area_name}:\n")
+            elif (new_area_name != current_area_name):
+                file.write(f"\n{new_area_name}:\n")
 
-                if "de Lower Jail" in map_verbose_name: # map name fix
-                    map_verbose_name = "Outside Lower Jail"
-                if current_item_name in verbose_item_names:
-                    current_item_name = verbose_item_names.get(current_item_name)
-                if (    node.map_area.name in verbose_item_locations
-                    and node.key_name_item in verbose_item_locations.get(node.map_area.name)):
-                    item_location = (f"{map_verbose_name} - "
-                                     f"{verbose_item_locations.get(node.map_area.name).get(node.key_name_item)}")
-                else:
-                    item_location = f"{map_verbose_name} - {node.key_name_item}"
+            current_item_name = node.current_item.item_name
+            map_verbose_name = node.map_area.verbose_name
 
-                file.write(f"    ({item_location}): {current_item_name}\n")
-                current_area_name = verbose_area_names.get(node.map_area.name[:3])
-        else:
-            for node in sorted_by_area:
-                file.write(f"[{node.map_area.name}] {node.key_name_item} - "
-                           f"{node.current_item.item_name}\n")
+            if "de Lower Jail" in map_verbose_name: # map name fix
+                map_verbose_name = "Outside Lower Jail"
+            if current_item_name in verbose_item_names:
+                current_item_name = verbose_item_names.get(current_item_name)
+            if (    node.map_area.name in verbose_item_locations
+                and node.key_name_item in verbose_item_locations.get(node.map_area.name)):
+                item_location = (f"{map_verbose_name} - "
+                                    f"{verbose_item_locations.get(node.map_area.name).get(node.key_name_item)}")
+            else:
+                item_location = f"{map_verbose_name} - {node.key_name_item}"
+
+            file.write(f"    ({item_location}): {current_item_name}\n")
+            current_area_name = verbose_area_names.get(node.map_area.name[:3])
+    else:
+        for node in sorted_by_area:
+            file.write(f"[{node.map_area.name}] {node.key_name_item} - "
+                        f"{node.current_item.item_name}\n")
+
+    if is_web_spoiler_log:
+        file.seek(0)
+        return file
