91844d15d79ac8960a9f06ec0f2722a5864e9c0b
Icebound777 <frozenblue@gmx.de>
2022-04-30 00:22:34 +0200

    keep item traps out of shiver mountain sockets

--- metadata/node_exclusion.py
+++ metadata/node_exclusion.py
@@ -1,9 +1,12 @@
-# Item nodes that are underwater
+# Item nodes that are underwater or the Shiver Mountain sockets
 exclude_from_trap_placement = [
-    "JAN_06/ItemA",
-    "JAN_08/ItemA",
-    "JAN_08/ItemB",
-    "JAN_08/ItemC",
-    "JAN_10/ItemA",
-    "SAM_11/ItemA"
+    "JAN_06/ItemA", # underwater
+    "JAN_08/ItemA", # underwater
+    "JAN_08/ItemB", # underwater
+    "JAN_08/ItemC", # underwater
+    "JAN_10/ItemA", # underwater
+    "SAM_11/ItemA", # underwater
+    "SAM_09/ItemA", # Shiver Mountain sockets
+    "SAM_09/ItemB", # Shiver Mountain sockets
+    "SAM_09/ItemC", # Shiver Mountain sockets
 ]
