c208cd96f86087b5459ce4ba2afdeb5f59ab46ab
Icebound777 <frozenblue@gmx.de>
2022-04-30 00:42:20 +0200

    undoing last commit; fixed on mod repo

--- metadata/node_exclusion.py
+++ metadata/node_exclusion.py
@@ -1,4 +1,4 @@
-# Item nodes that are underwater or the Shiver Mountain sockets
+# Item nodes that are underwater
 exclude_from_trap_placement = [
     "JAN_06/ItemA", # underwater
     "JAN_08/ItemA", # underwater
@@ -6,7 +6,4 @@ exclude_from_trap_placement = [
     "JAN_08/ItemC", # underwater
     "JAN_10/ItemA", # underwater
     "SAM_11/ItemA", # underwater
-    "SAM_09/ItemA", # Shiver Mountain sockets
-    "SAM_09/ItemB", # Shiver Mountain sockets
-    "SAM_09/ItemC", # Shiver Mountain sockets
 ]
