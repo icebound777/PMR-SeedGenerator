60dc8371df4b1ff14081ca8d672dbdc955f076f1
Icebound777 <frozenblue@gmx.de>
2022-05-08 23:49:24 +0200

    exclude coins over spinning flowers from traps

--- metadata/node_exclusion.py
+++ metadata/node_exclusion.py
@@ -1,5 +1,9 @@
-# Item nodes that are underwater
+# Item nodes that are underwater or over spinning flowers
 exclude_from_trap_placement = [
+    "JAN_00/ItemA", # over spinningflower
+    "JAN_00/ItemB", # over spinningflower
+    "JAN_01/ItemB", # over spinningflower
+    "JAN_01/ItemC", # over spinningflower
     "JAN_06/ItemA", # underwater
     "JAN_08/ItemA", # underwater
     "JAN_08/ItemB", # underwater
