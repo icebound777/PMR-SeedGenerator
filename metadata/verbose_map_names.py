c58bc0032e43c35d5784ba481b25a0b585a10d67
Icebound777 <frozenblue@gmx.de>
2022-07-16 01:27:05 +0200

    fix(log): Clearer TIK Map Names

--- metadata/verbose_map_names.py
+++ metadata/verbose_map_names.py
@@ -3,6 +3,8 @@ map_name_corrections = {
     "Bee Hive (Path Splits)": "Flowers Vanish",
     "Flowers Vanish": "Laughing Rock",
     "Laughing Rock": "Bee Hive (HP Plus)",
+    "Block Puzzle Room (B2)": "Blue Pushblock Room (B2)",
+    "Hidden Blocks Room (B2)": "Bridge to Shiver City (B2)",
     "Windy Path (B3)": "Winding Path (Spiny Room)",
     "Outside Lower Jail (No Lava)": "Outside Lower Jail",
     "Outisde Lower Jail (Lava)": "Outside Lower Jail",
