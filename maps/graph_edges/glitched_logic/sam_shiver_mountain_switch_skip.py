593d6b4654e7afa2aff78ca4b7cf9f2ece50a609
Pronyo <franktb93@gmail.com>
2022-09-04 10:46:32 -0400

    Add missing laki requirement for shiver mtn switch skip

--- maps/graph_edges/glitched_logic/sam_shiver_mountain_switch_skip.py
+++ maps/graph_edges/glitched_logic/sam_shiver_mountain_switch_skip.py
@@ -4,8 +4,8 @@ for Glitched Logic: Shiver Mountain Switch Skip
 """
 edges_sam_add_shiver_mountain_switch_skip= [    
     #? Shiver Mountain Passage Mountain Entrance -> Shiver Mountain Passage Exit East
-    {"from": {"map": "SAM_07", "id": 0}, "to": {"map": "SAM_07", "id": 1}, "reqs": [], "mapchange": False},
+    {"from": {"map": "SAM_07", "id": 0}, "to": {"map": "SAM_07", "id": 1}, "reqs": [["Lakilester"]], "mapchange": False},
 
     #? Shiver Mountain Passage Exit East -> Shiver Mountain Passage Mountain Entrance
-    {"from": {"map": "SAM_07", "id": 1}, "to": {"map": "SAM_07", "id": 0}, "reqs": [], "mapchange": False},
+    {"from": {"map": "SAM_07", "id": 1}, "to": {"map": "SAM_07", "id": 0}, "reqs": [["Lakilester"]], "mapchange": False},
 ]
