b3e8942f657392ee4f00592006798ac542ff4ffc
Icebound777 <frozenblue@gmx.de>
2022-09-21 21:11:38 +0200

    adjust: sell price for gear items same as keyitems

--- rando_modules/random_shop_prices.py
+++ rando_modules/random_shop_prices.py
@@ -92,7 +92,7 @@ def get_shop_price(
                 if (buy_price - (buy_price % 5)) != 0:
                     buy_price = round(buy_price / 5) * 5
 
-            elif item_type in ["BADGE", "KEYITEM", "PARTNER"]:
+            elif item_type in ["BADGE", "KEYITEM", "PARTNER", "GEAR"]:
                 buy_price = random.choice([10,15,20,25,30])
 
             elif item_type == "COIN":
