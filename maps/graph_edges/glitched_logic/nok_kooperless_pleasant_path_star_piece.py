247bd4d1a846b7c7a1497104125ffe1cdc540f68
Pronyo <franktb93@gmail.com>
2022-10-22 07:29:20 -0400

    fix nok kooperless star piece (missing edge)

--- maps/graph_edges/glitched_logic/nok_kooperless_pleasant_path_star_piece.py
+++ maps/graph_edges/glitched_logic/nok_kooperless_pleasant_path_star_piece.py
@@ -5,4 +5,7 @@ for Glitched Logic: Kooper-less Pleasant Path Star Piece.
 edges_nok_add_kooperless_starpiece_clippy = [
     #* Pleasant Path Bridge Exit Right -> ItemA (StarPiece)
     {"from": {"map": "NOK_12", "id": 1}, "to": {"map": "NOK_12", "id": "ItemA"}, "reqs": [["Lakilester"],["Boots"]], "mapchange": False},
+
+    #* Pleasant Path Bridge Exit Right -> ItemA (StarPiece)
+    {"from": {"map": "NOK_12", "id": 1}, "to": {"map": "NOK_12", "id": "ItemA"}, "reqs": [["Lakilester"],["Boots"]], "mapchange": False},
 ]
