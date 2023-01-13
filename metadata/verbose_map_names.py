a973b4609a749f8ea4826c49a9722809e69e743c
Icebound777 <frozenblue@gmx.de>
2023-01-13 22:07:09 +0100

    log: adjust a few area and map names

--- metadata/verbose_map_names.py
+++ metadata/verbose_map_names.py
@@ -3,9 +3,12 @@ map_name_corrections = {
     "Bee Hive (Path Splits)": "Flowers Vanish",
     "Flowers Vanish": "Laughing Rock",
     "Laughing Rock": "Bee Hive (HP Plus)",
+    "Escape Scene": "Outside Tubbas Castle",
     "Block Puzzle Room (B2)": "Blue Pushblock Room (B2)",
     "Hidden Blocks Room (B2)": "Bridge to Shiver City (B2)",
+    "Path to Shiver City (B2)": "Pipe to Frozen Room (B2)",
     "Windy Path (B3)": "Winding Path (Spiny Room)",
+    "Hanger": "Hangar",
     "Outside Lower Jail (No Lava)": "Outside Lower Jail",
     "Outisde Lower Jail (Lava)": "Outside Lower Jail",
 }
