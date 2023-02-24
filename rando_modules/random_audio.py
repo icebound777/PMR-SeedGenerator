3874e51c17d1c7890681f47f10ea68c5eefda1dd
Icebound777 <frozenblue@gmx.de>
2023-02-24 22:16:40 +0100

    chore: remove unused audio mute function

--- rando_modules/random_audio.py
+++ rando_modules/random_audio.py
@@ -6,16 +6,3 @@ DBValue: (s16 song)(s16 variation)
 I.e. to map song 4, variation 1 to song F, variation 2 we'd have
 A7000401 : 000F0002
 """
-
-def get_turned_off_music():
-    """
-    Returns a list of tuples where the first value holds the dbkey for a song
-    and the second value is -1, which mutes the song.
-    """
-    music_list = []
-
-    for song_id in range(0, 0x97):
-        db_key = 0xA7000000 + (song_id << 8)
-        music_list.append((int(db_key), 0xFFFF0000))
-
-    return music_list
