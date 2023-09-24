a18d5c6ca7cea055d0f1f28c49f356441ba9c24b
Icebound777 <frozenblue@gmx.de>
2023-09-24 02:32:09 +0200

    feat: Laki Jail Break: glitch through cracked wall

--- maps/graph_edges/glitched_logic/trd_laki_jailbreak.py
+++ maps/graph_edges/glitched_logic/trd_laki_jailbreak.py
@@ -7,4 +7,6 @@ edges_trd_add_laki_jailbreak = [
     {"from": {"map": "TRD_03", "id": 0}, "to": {"map": "TRD_03", "id": "ItemA"}, "reqs": [["Lakilester"]], "mapchange": False},
     #?ItemA (KoopaFortressKey) ->  Central Hall Exit Top Left
     {"from": {"map": "TRD_03", "id": "ItemA"}, "to": {"map": "TRD_03", "id": 2}, "reqs": [["Lakilester"],["can_climb_steps"]], "mapchange": False},
+    # Central Hall Exit Left Cell -> Left Stairway Exit Middle Right
+    {"from": {"map": "TRD_03", "id": 4}, "to": {"map": "TRD_02", "id": 4}, "reqs": [["Lakilester"],["can_climb_steps"]]},
 ]
