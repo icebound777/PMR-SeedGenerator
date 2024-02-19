3beda5a11d3a65354425ea688a4156e109edc117
Icebound777 <frozenblue@gmx.de>
2024-02-19 21:49:55 +0100

    feat: add per-seed randomness: boo ring 1

--- rando_modules/random_puzzles_minigames.py
+++ rando_modules/random_puzzles_minigames.py
@@ -19,5 +19,13 @@ def get_puzzles_minigames() -> list:
         if puzzle.name == "BooRingOBK04":
             num_throws = random.randint(6, 10)
             puzzle_minigame_list.append((puzzle.get_key(), num_throws))
+        # Record Boo Ring: Degrees of rotation until item drop
+        elif puzzle.name == "BooRingOBK08Degrees":
+            degrees = random.randint(180, 540)
+            puzzle_minigame_list.append((puzzle.get_key(), degrees))
+        # Record Boo Ring: Delay until main Boo stops
+        elif puzzle.name == "BooRingOBK08Delay":
+            delay = random.randint(360, 380)
+            puzzle_minigame_list.append((puzzle.get_key(), delay))
 
     return puzzle_minigame_list
