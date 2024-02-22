fbc65e9225de6d2d0b8e9881b17a26701a9df846
Icebound777 <frozenblue@gmx.de>
2024-02-22 23:49:05 +0100

    feat: add random puzzle: albino dinos

--- rando_modules/random_puzzles_minigames.py
+++ rando_modules/random_puzzles_minigames.py
@@ -19,25 +19,68 @@ def get_puzzles_minigames() -> list:
         if puzzle.name == "FuzzyTreesRound1":
             num_hops = random.randint(10, 13)
             puzzle_minigame_list.append((puzzle.get_key(), num_hops))
+
         # Fuzzy Tree Minigame Round 2 hops
         elif puzzle.name == "FuzzyTreesRound2":
             num_hops = random.randint(9, 12)
             puzzle_minigame_list.append((puzzle.get_key(), num_hops))
+
         # Fuzzy Tree Minigame Round 3 hops
         elif puzzle.name == "FuzzyTreesRound3":
             num_hops = random.randint(8, 11)
             puzzle_minigame_list.append((puzzle.get_key(), num_hops))
+
         # Super Boots Chest Boo Ring
         elif puzzle.name == "BooRingOBK04":
             num_throws = random.randint(6, 10)
             puzzle_minigame_list.append((puzzle.get_key(), num_throws))
+
         # Record Boo Ring: Degrees of rotation until item drop
         elif puzzle.name == "BooRingOBK08Degrees":
             degrees = random.randint(180, 540)
             puzzle_minigame_list.append((puzzle.get_key(), degrees))
+
         # Record Boo Ring: Delay until main Boo stops
         elif puzzle.name == "BooRingOBK08Delay":
             delay = random.randint(360, 380)
             puzzle_minigame_list.append((puzzle.get_key(), delay))
 
+        # Albino Dino Statues: Initial positions
+        elif puzzle.name == "AlbinoDinoPositions":
+            positions_encoded = _albino_dino_puzzle()
+            puzzle_minigame_list.append((puzzle.get_key(), positions_encoded))
+
     return puzzle_minigame_list
+
+
+def _albino_dino_puzzle() -> int:
+    max_x_coord = 8
+    max_z_coord = 2
+
+    dino_1 = (random.randint(0, max_x_coord), random.randint(0, max_z_coord))
+
+    # make sure the dinos don't share a space
+    while True:
+        dino_2 = (random.randint(0, max_x_coord), random.randint(0, max_z_coord))
+        if dino_2[0] == dino_1[0] and dino_2[1] == dino_1[1]:
+            continue
+        else:
+            break
+    while True:
+        dino_3 = (random.randint(0, max_x_coord), random.randint(0, max_z_coord))
+        if dino_3[0] == dino_1[0] and dino_3[1] == dino_1[1]:
+            continue
+        elif dino_3[0] == dino_2[0] and dino_3[1] == dino_2[1]:
+            continue
+        else:
+            break
+
+    # Hex-Encoding: 0x00xzxzxz
+    return (
+        (dino_1[0] << 20)
+      + (dino_1[1] << 16)
+      + (dino_2[0] << 12)
+      + (dino_2[1] << 8)
+      + (dino_3[0] << 4)
+      + dino_3[1]
+    )
