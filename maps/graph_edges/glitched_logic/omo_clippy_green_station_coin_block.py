bb888de2e0d1346de5526e99dcdf5c9f2510ead9
Phantom <phantom@phantom-games.com>
2024-06-03 13:39:40 -0700

    small PR feedback

--- maps/graph_edges/glitched_logic/omo_clippy_green_station_coin_block.py
+++ maps/graph_edges/glitched_logic/omo_clippy_green_station_coin_block.py
@@ -4,5 +4,5 @@ for Glitched Logic: Clippy Green Station Coin Block.
 """
 edges_omo_clippy_green_station_coin_block = [
     #* GRN Treadmills/Slot Machine -> RandomBlockItemA (MultiCoinBlock)
-    (0xA1100840, {"from": {"map": "OMO_09", "id": "ItemH"}, "to": {"map": "OMO_09", "id": "RandomBlockItemA"}, "reqs": [["Lakilester"],["Boots"]], "mapchange": False})
+    (0xA1100840, {"from": {"map": "OMO_09", "id": "ItemH"}, "to": {"map": "OMO_09", "id": "RandomBlockItemA"}, "reqs": [["Lakilester"]], "mapchange": False})
 ]
