aed4906682ff02539ac5bf38361eb5a396e80845
Phantom <phantom@phantom-games.com>
2024-11-15 15:56:22 -0800

    Reworded comment for clarity

--- maps/graph_edges/glitched_logic/mac_jumpless_dane_t_letters.py
+++ maps/graph_edges/glitched_logic/mac_jumpless_dane_t_letters.py
@@ -1,8 +1,8 @@
 """
 This file represents edges of the world graph that have to be added
 for Glitched Logic: Jumpless Dane T Letters.
-From the train station platform, Parakarry can be used to cross over to the opposite ledge
-before the camera turns, allowing Dane T's two letters to be delivered without boots.
+From the train station platform, Parakarry can be used to climb the opposite ledge before
+the camera finishes turning, allowing Dane T's two letters to be delivered without boots.
 """
 edges_mac_jumpless_dane_t_letters = [
     #* Station District -> Dane T letter turn in
