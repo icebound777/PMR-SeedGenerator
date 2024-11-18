514020f1c0bf3144be11686fc6be4b97935c2abb
Phantom <phantom@phantom-games.com>
2024-11-18 02:46:07 -0800

    Fix edges for basement skip

--- maps/graph_edges/glitched_logic/kpa_basement_skip.py
+++ maps/graph_edges/glitched_logic/kpa_basement_skip.py
@@ -4,15 +4,21 @@ for Glitched Logic: Basement Skip
 """
 edges_kpa_add_basement_skip_parakarry = [
     #? Front Door Exterior Lava Door Right -> Front Door Exterior Hangar Door Bottom Left (ultra boots clip and parakarry)
-    {"from": {"map": "KPA_62",  "id": 2}, "to": {"map": "KPA_62",  "id": 0}, "reqs": [["Lakilester","GF_KPA16_ShutOffLava"],["Parakarry"],["UltraBoots"]], "mapchange": False}, 
+    {"from": {"map": "KPA_62",  "id": 2}, "to": {"map": "KPA_62",  "id": 0}, "reqs": [["Lakilester","GF_KPA16_ShutOffLava"],["Parakarry"],["UltraBoots"]], "pseudoitems": ["RF_Ch8_FirstGuardDoor"], "mapchange": False}, 
+    #? Guard Door 1 Door Left -> Guard Door 1 Fall In Trap Door
+    {"from": {"map": "KPA_81",  "id": 0}, "to": {"map": "KPA_81",  "id": 1}, "reqs": [["Bombette"], ["Lakilester"], ["Parakarry"], ["UltraBoots"]], "mapchange": False}, 
 ]
 
 edges_kpa_add_basement_skip_lakilester = [
     #? Front Door Exterior Lava Door Right -> Front Door Exterior Hangar Door Bottom Left (laki teleport)
-    {"from": {"map": "KPA_62",  "id": 2}, "to": {"map": "KPA_62",  "id": 0}, "reqs": [["Lakilester"],["can_climb_steps"]], "mapchange": False}, 
+    {"from": {"map": "KPA_62",  "id": 2}, "to": {"map": "KPA_62",  "id": 0}, "reqs": [["Lakilester"],["can_climb_steps"]], "pseudoitems": ["RF_Ch8_FirstGuardDoor"], "mapchange": False}, 
+    #? Guard Door 1 Door Left -> Guard Door 1 Fall In Trap Door
+    {"from": {"map": "KPA_81",  "id": 0}, "to": {"map": "KPA_81",  "id": 1}, "reqs": [["Bombette"], ["Lakilester"], ["can_climb_steps"]], "mapchange": False}, 
 ]
 
 edges_kpa_add_basement_skip_hammer = [
     #? Front Door Exterior Lava Door Right -> Front Door Exterior Hangar Door Bottom Left (hammer clip)
-    {"from": {"map": "KPA_62",  "id": 2}, "to": {"map": "KPA_62",  "id": 0}, "reqs": [["Lakilester","GF_KPA16_ShutOffLava"],["can_climb_steps"],["Hammer"]], "mapchange": False}, 
+    {"from": {"map": "KPA_62",  "id": 2}, "to": {"map": "KPA_62",  "id": 0}, "reqs": [["Lakilester","GF_KPA16_ShutOffLava"],["can_climb_steps"],["Hammer"]], "pseudoitems": ["RF_Ch8_FirstGuardDoor"], "mapchange": False}, 
+    #? Guard Door 1 Door Left -> Guard Door 1 Fall In Trap Door
+    {"from": {"map": "KPA_81",  "id": 0}, "to": {"map": "KPA_81",  "id": 1}, "reqs": [["Bombette"], ["Lakilester"], ["Hammer"], ["can_climb_steps"]], "mapchange": False}, 
 ]
