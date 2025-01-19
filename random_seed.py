icebound777/PMR-SeedGenerator

commit c40089be9bdeed310bb28a959a6ba7da5fd9779a

feat: make power stars shop price dynamic

random_seed.py
@@ -369,7 +369,8 @@ class RandomSeed:
                 node.current_item.base_price = get_shop_price(
                     node,
                     logic_settings.include_shops,
-                    self.rando_settings.merlow_reward_pricing
+                    self.rando_settings.merlow_reward_pricing,
+                    self.logic_settings.star_hunt_total,
                 )
 
         # Modify Mystery? item

rando_modules/random_shop_prices.py
@@ -8,7 +8,8 @@ from rando_enums.enum_options import MerlowRewardPricing
 def get_shop_price(
     node:Node,
     do_randomize_shops:bool,
-    merlow_costs:int
+    merlow_costs:int,
+    power_stars_in_seed:int,
 ) -> int:
     """
     Return the price for an item for offer within a shop (regular or Merlow's).
@@ -80,8 +81,16 @@ def get_shop_price(
                 if (buy_price - (buy_price % 5)) != 0:
                     buy_price = round(buy_price / 5) * 5
 
-            elif item_type in ["BADGE", "KEYITEM", "PARTNER", "GEAR", "POWERSTAR", "PARTNERUPGRADE"]:
+            elif item_type in ["BADGE", "KEYITEM", "PARTNER", "GEAR", "PARTNERUPGRADE"]:
                 buy_price = random.choice([10,15,20,25,30])
+
+            elif item_type == "POWERSTAR":
+                if power_stars_in_seed <= 40:
+                    buy_price = random.choice([10,15,20,25,30])
+                elif power_stars_in_seed <= 80:
+                    buy_price = random.choice([5,10,15,20])
+                else:
+                    buy_price = random.choice([5,10])
 
             elif item_type == "COIN":
                 buy_price = 1

commit c9f9d13312d7c333367ea49e57adca7d41acf65e

fix: broken generation due to wrong field access
Regression from c40089be

random_seed.py
@@ -371,7 +371,7 @@ class RandomSeed:
                     node,
                     logic_settings.include_shops,
                     self.rando_settings.merlow_reward_pricing,
-                    self.logic_settings.star_hunt_total,
+                    logic_settings.star_hunt_total,
                 )
 
         # Modify Mystery? item