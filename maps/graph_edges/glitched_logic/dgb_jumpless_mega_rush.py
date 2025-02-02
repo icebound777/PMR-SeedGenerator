e16c1781a99137b4f85e5fd283c3c6728a6c7a43
Phantom <phantom@phantom-games.com>
2025-02-02 08:46:35 -0800

    Jumpless coins in mega rush room

--- maps/graph_edges/glitched_logic/dgb_jumpless_mega_rush.py
+++ maps/graph_edges/glitched_logic/dgb_jumpless_mega_rush.py
@@ -6,4 +6,16 @@ Can climb the bookshelf by partially clipping into each shelf and using Parakarr
 edges_dgb_add_jumpless_mega_rush = [
     #* Hidden Bedroom (2F) Door Bottom -> ItemA (MegaRush)
     {"from": {"map": "DGB_13", "id": 0}, "to": {"map": "DGB_13", "id": "ItemA"}, "reqs": [["Parakarry"]], "mapchange": False},
+    #* Hidden Bedroom (2F) Door Bottom -> ItemB (Coin)
+    {"from": {"map": "DGB_13", "id": 0}, "to": {"map": "DGB_13", "id": "ItemB"}, "reqs": [["Parakarry"]], "mapchange": False},
+    #* Hidden Bedroom (2F) Door Bottom -> ItemC (Coin)
+    {"from": {"map": "DGB_13", "id": 0}, "to": {"map": "DGB_13", "id": "ItemC"}, "reqs": [["Parakarry"]], "mapchange": False},
+    #* Hidden Bedroom (2F) Door Bottom -> ItemD (Coin)
+    {"from": {"map": "DGB_13", "id": 0}, "to": {"map": "DGB_13", "id": "ItemD"}, "reqs": [["Parakarry"]], "mapchange": False},
+    #* Hidden Bedroom (2F) Door Bottom -> ItemE (Coin)
+    {"from": {"map": "DGB_13", "id": 0}, "to": {"map": "DGB_13", "id": "ItemE"}, "reqs": [["Parakarry"]], "mapchange": False},
+    #* Hidden Bedroom (2F) Door Bottom -> ItemF (Coin)
+    {"from": {"map": "DGB_13", "id": 0}, "to": {"map": "DGB_13", "id": "ItemF"}, "reqs": [["Parakarry"]], "mapchange": False},
+    #* Hidden Bedroom (2F) Door Bottom -> ItemG (Coin)
+    {"from": {"map": "DGB_13", "id": 0}, "to": {"map": "DGB_13", "id": "ItemG"}, "reqs": [["Parakarry"]], "mapchange": False},
 ]
