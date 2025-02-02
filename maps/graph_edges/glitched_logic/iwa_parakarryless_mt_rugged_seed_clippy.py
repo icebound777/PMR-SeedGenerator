1b72efa3ac9f4792b6a5930323a61d973df28c8d
Phantom <phantom@phantom-games.com>
2025-02-02 08:42:03 -0800

    Clippy to mt rugged magical seed from above

--- maps/graph_edges/glitched_logic/iwa_parakarryless_mt_rugged_seed_clippy.py
+++ maps/graph_edges/glitched_logic/iwa_parakarryless_mt_rugged_seed_clippy.py
@@ -5,4 +5,6 @@ for Glitched Logic: Parakarry-less Mt. Rugged Seed using Clippy.
 edges_iwa_add_parakarryless_mt_rugged_seed_clippy = [
     #* Mt Rugged 3 Exit Bottom Left -> GiftA (MagicalSeed2)
     {"from": {"map": "IWA_02", "id": 0},"to": {"map": "IWA_02", "id": "GiftA"}, "reqs": [["Boots"], ["Lakilester"]], "mapchange" : False},
+    #* Mt Rugged 3 Exit Top Right -> GiftA (MagicalSeed2)
+    {"from": {"map": "IWA_02", "id": 2},"to": {"map": "IWA_02", "id": "GiftA"}, "reqs": [["Lakilester"]], "mapchange" : False},
 ]
