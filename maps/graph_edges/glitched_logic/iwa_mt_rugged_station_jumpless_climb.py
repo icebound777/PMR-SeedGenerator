e1567417603ff84c86c6bd84a24fc17add5a1a31
Phantom <phantom@phantom-games.com>
2025-04-03 10:38:47 -0700

    Add the super block to jumpless mt rugged laki climb

--- maps/graph_edges/glitched_logic/iwa_mt_rugged_station_jumpless_climb.py
+++ maps/graph_edges/glitched_logic/iwa_mt_rugged_station_jumpless_climb.py
@@ -8,11 +8,15 @@ edges_iwa_add_mt_rugged_station_jumpless_climb_bombette= [
 ]
 
 edges_iwa_add_mt_rugged_station_jumpless_climb_laki_block_clip= [
-    #? Train Station Ride The Train -> Train Station Exit Top Right
-    {"from": {"map": "IWA_10", "id": 0}, "to": {"map": "IWA_10", "id": 1}, "reqs": [["Lakilester"], ["SpeedySpin"], ["Hammer"]], "mapchange": False},
+    #* Train Station Ride The Train -> RandomBlockItemA (GenericUpgrade)
+    {"from": {"map": "IWA_10", "id": 0}, "to": {"map": "IWA_10", "id": "RandomBlockItemA"}, "reqs": [["Lakilester"], ["can_hit_floating_blocks"]], "mapchange": False},
+    #? RandomBlockItemA (GenericUpgrade) -> Train Station Exit Top Right
+    {"from": {"map": "IWA_10", "id": "RandomBlockItemA"}, "to": {"map": "IWA_10", "id": 1}, "reqs": [["Lakilester"], ["SpeedySpin"], ["Hammer"]], "mapchange": False},
 ]
 
 edges_iwa_add_mt_rugged_station_jumpless_climb_laki= [
+    #* Train Station Ride The Train -> RandomBlockItemA (GenericUpgrade)
+    {"from": {"map": "IWA_10", "id": 0}, "to": {"map": "IWA_10", "id": "RandomBlockItemA"}, "reqs": [["Lakilester"], ["can_hit_floating_blocks"]], "mapchange": False},
     #? Train Station Ride The Train -> Train Station Exit Top Right
     {"from": {"map": "IWA_10", "id": 0}, "to": {"map": "IWA_10", "id": 1}, "reqs": [["Lakilester"], ["SpeedySpin"]], "mapchange": False},
 ]
