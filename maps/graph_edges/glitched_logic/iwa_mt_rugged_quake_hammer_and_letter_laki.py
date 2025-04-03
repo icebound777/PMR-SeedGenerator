70b53ba42738878d57cc21599edb5992458d5313
Phantom <phantom@phantom-games.com>
2025-04-03 09:42:00 -0700

    Remove jumpless laki teleport from logic for quake hammer and letter

--- maps/graph_edges/glitched_logic/iwa_mt_rugged_quake_hammer_and_letter_laki.py
+++ maps/graph_edges/glitched_logic/iwa_mt_rugged_quake_hammer_and_letter_laki.py
@@ -5,10 +5,6 @@ for Glitched Logic: Mt. Rugged Quake Hammer + Letter with Laki.
 edges_iwa_add_quake_hammer_and_letter_laki = [
     #* ItemB (Letter01) -> ItemA (QuakeHammer)
     {"from": {"map": "IWA_01", "id": "ItemB"}, "to": {"map": "IWA_01", "id": "ItemA"}, "reqs": [["Lakilester"]], "mapchange": False},
-    #* Mt Rugged 2 Exit Bottom Left -> ItemB (Letter01)
-    {"from": {"map": "IWA_01", "id": 0}, "to": {"map": "IWA_01", "id": "ItemB"}, "reqs": [["Lakilester"]], "mapchange": False},
-    #* Mt Rugged 2 Exit Top Left -> ItemB (Letter01)
-    {"from": {"map": "IWA_01", "id": 1}, "to": {"map": "IWA_01", "id": "ItemB"}, "reqs": [["Lakilester"]], "mapchange": False},
     #* Mt Rugged 2 Exit Bottom Right -> ItemB (Letter01)
     {"from": {"map": "IWA_01", "id": 2}, "to": {"map": "IWA_01", "id": "ItemB"}, "reqs": [["Lakilester"]], "mapchange": False},
 ]
