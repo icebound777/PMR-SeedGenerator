0a3982edbdb59e3abc72496ad27a67e5278c92a1
Icebound777 <frozenblue@gmx.de>
2025-09-05 22:06:32 +0200

    fix: stone block w/ boots ignoring hidden blocks

--- maps/graph_edges/glitched_logic/global_break_stone_blocks_with_ultra_boots.py
+++ maps/graph_edges/glitched_logic/global_break_stone_blocks_with_ultra_boots.py
@@ -4,7 +4,7 @@ for Glitched Logic: Break Stone Blocks With Ultra Boots.
 """
 edges_kmr_tik_isk_add_break_stone_blocks_ultra_boots = [
     #* Bottom of the Cliff Exit Right -> HiddenYBlockA (RepelGel)
-    {"from": {"map": "KMR_03", "id": 1}, "to": {"map": "KMR_03", "id": "HiddenYBlockA"}, "reqs": [["UltraBoots"]], "mapchange": False},
+    {"from": {"map": "KMR_03", "id": 1}, "to": {"map": "KMR_03", "id": "HiddenYBlockA"}, "reqs": [["UltraBoots"],["can_see_hidden_blocks"]], "mapchange": False},
     #? Sewer Entrance (B1) Green Pipe Left -> Sewer Entrance (B1) Exit Left
     {"from": {"map": "TIK_06", "id": 2}, "to": {"map": "TIK_06", "id": 0}, "reqs": [["UltraBoots"]], "mapchange": False},
     #* Sand Drainage Room 3 Exit Bottom Left -> ItemA (RuinsKey)
