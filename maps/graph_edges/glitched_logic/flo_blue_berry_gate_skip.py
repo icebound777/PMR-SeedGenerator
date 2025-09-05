5d13ba0eabd254697087421f807aba32c95ef12b
Icebound777 <frozenblue@gmx.de>
2025-09-05 17:33:07 +0200

    fix: Blue Berry Gate Skip Bombette bi-directional

--- maps/graph_edges/glitched_logic/flo_blue_berry_gate_skip.py
+++ maps/graph_edges/glitched_logic/flo_blue_berry_gate_skip.py
@@ -12,6 +12,4 @@ edges_flo_add_blue_berry_gate_skip_laki = [
 edges_flo_add_blue_berry_gate_skip_bombette_push = [
     #? (West) Path to Maze Exit Right -> (West) Path to Maze Exit Left
     {"from": {"map": "FLO_23", "id": 0}, "to": {"map": "FLO_23", "id": 1}, "reqs": [["Bombette"]], "mapchange": False},
-    #? (West) Path to Maze Exit Left -> (West) Path to Maze Exit Right
-    {"from": {"map": "FLO_23", "id": 1}, "to": {"map": "FLO_23", "id": 0}, "reqs": [], "mapchange": False},
 ]
