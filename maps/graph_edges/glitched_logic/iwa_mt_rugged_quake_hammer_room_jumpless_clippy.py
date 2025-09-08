48c44d96ba9599bae9d4007bbe373ae4dea8bd5c
Icebound777 <frozenblue@gmx.de>
2025-09-08 21:37:07 +0200

    refactor: optimize jumpless iwa_01

--- maps/graph_edges/glitched_logic/iwa_mt_rugged_quake_hammer_room_jumpless_clippy.py
+++ maps/graph_edges/glitched_logic/iwa_mt_rugged_quake_hammer_room_jumpless_clippy.py
@@ -6,16 +6,10 @@ with a laki teleport followed by using Clippy and falling out of bounds.
 The clippy portion can be found in this video from mrzebra: https://youtu.be/yLdXnkNiXPs
 """
 edges_iwa_add_quake_hammer_room_jumpless_clippy = [
-    #? Mt Rugged 2 Exit Bottom Left -> Mt Rugged 2 Exit Top Right
-    {"from": {"map": "IWA_01", "id": 0}, "to": {"map": "IWA_01", "id": 3}, "reqs": [["Lakilester"]], "mapchange": False},
-    #? Mt Rugged 2 Exit Top Left -> Mt Rugged 2 Exit Top Right
-    {"from": {"map": "IWA_01", "id": 1}, "to": {"map": "IWA_01", "id": 3}, "reqs": [["Lakilester"]], "mapchange": False},
     #? Mt Rugged 2 Exit Bottom Right -> Mt Rugged 2 Exit Top Right
     {"from": {"map": "IWA_01", "id": 2}, "to": {"map": "IWA_01", "id": 3}, "reqs": [["Lakilester"]], "mapchange": False},
     #? Mt Rugged 2 Exit Bottom Left -> Mt Rugged 2 Exit Bottom Right
     {"from": {"map": "IWA_01", "id": 0}, "to": {"map": "IWA_01", "id": 2}, "reqs": [["Lakilester"]], "mapchange": False},
     #? Mt Rugged 2 Exit Top Left -> Mt Rugged 2 Exit Bottom Right
     {"from": {"map": "IWA_01", "id": 1}, "to": {"map": "IWA_01", "id": 2}, "reqs": [["Lakilester"]], "mapchange": False},
-    #? Mt Rugged 2 Exit Top Right -> Mt Rugged 2 Exit Bottom Right
-    {"from": {"map": "IWA_01", "id": 3}, "to": {"map": "IWA_01", "id": 2}, "reqs": [["Lakilester"]], "mapchange": False},
 ]
