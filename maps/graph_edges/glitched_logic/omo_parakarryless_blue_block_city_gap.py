20c2ac65eed1ae4c68152a83eccff144a7968d00
Icebound777 <frozenblue@gmx.de>
2025-12-03 21:55:38 +0100

    docs: minor glitch commt fixes

--- maps/graph_edges/glitched_logic/omo_parakarryless_blue_block_city_gap.py
+++ maps/graph_edges/glitched_logic/omo_parakarryless_blue_block_city_gap.py
@@ -1,6 +1,6 @@
 """
 This file represents edges of the world graph that have to be added
-for Glitched Logic: Parakaryless Blue Block City Gap
+for Glitched Logic: Parakarryless Blue Block City Gap
 """
 edges_omo_add_parakarryless_blue_block_city_gap = [
      #* BLU Block City Exit West -> YBlockC (ThunderBolt)
